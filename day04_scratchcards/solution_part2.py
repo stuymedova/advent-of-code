0e923c9bea5cdbf9900b673d25c45ce626a57565
Sofya Tuymedova <stuymedova@gmail.com>
2024-07-28 00:43:46 +0300

    Day 4 Part 2

--- day04_scratchcards/solution_part2.py
+++ day04_scratchcards/solution_part2.py
@@ -4,7 +4,7 @@ import os
 
 def main():
     data = handle_input()
-    sum = get_cards_points_value(data)
+    sum = get_cards_count(data)
     print(sum)
 
 
@@ -23,16 +23,17 @@ def handle_input():
     return data
 
 
-def get_cards_points_value(data):
-    points_count = 0
-    for line in data:
+def get_cards_count(data):
+    cards = [1 for _ in range(len(data))]
+    for i, line in enumerate(data):
         _, line = line.split(':')
         winning_numbers, numbers_we_have = line.split('|')
         winning_numbers = set(map(lambda x: int(x), winning_numbers.split()))
         numbers_we_have = set(map(lambda x: int(x), numbers_we_have.split()))
         intersection_length = len(set.intersection(winning_numbers, numbers_we_have))
-        points_count += 2 ** (intersection_length - 1) if intersection_length > 0 else 0
-    return points_count
+        for j in range(1, intersection_length + 1):
+            cards[i + j] += 1 * cards[i]
+    return sum(cards)
 
 
 if __name__ == '__main__':
